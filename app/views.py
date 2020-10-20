c65777fe437d98db2c6fd31b467b30922c4c765c
NgocNam1512 <ngocnam3518@gmail.com>
2020-10-21 00:31:11 +0700

    filter for flashcard

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from app.models import Flashcard, Card
 from app.serializers import FlashcardSerializer, CardSerializer
 from app.permissions import IsOwnerOrReadOnly
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 
 class FlashcardList(generics.ListCreateAPIView):
     queryset = Flashcard.objects.all()
     serializer_class = FlashcardSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 class FlashcardDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -22,3 +24,7 @@ class CardDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+# class FlashcardSearch(generics.ListAPIView):
+#     queryset = Flashcard.objects.all()
+#     serializer_class = FlashcardSerializer
